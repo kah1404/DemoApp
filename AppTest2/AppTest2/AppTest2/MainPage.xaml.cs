9f1f634aa2a81afa07cb9113c4bd06196bf5d269
Kevin andre hansen <kevin-a-hansen@live.no>
2017-10-18 15:26:30 +0200

    jhgjghjhj

---
+++ AppTest2/AppTest2/AppTest2/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTest2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+
+
+
+
+//            var trulsmumrikken = new Label
+//            {
+//                Text = "bleh",
+//                HorizontalOptions = LayoutOptions.Center,
+//                VerticalOptions = LayoutOptions.Center,
+//
+//            };
+//
+//            Content = new StackLayout
+//            {
+//                Orientation = StackOrientation.Vertical,
+//
+//                Children =
+//                {
+//                    trulsmumrikken   
+//                }
+//
+//                
+//            };
+
+
+
+        }
+
+        private void Butters_OnClicked(object sender, EventArgs e)
+        {
+            ByttMeg.Text = Entry.Text;
+        }
+    }
+}
